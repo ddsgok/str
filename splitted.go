959e89ad15adfe6ac38087d78b93219012da3b11
Dênnis Dantas <ddspog@gmail.com>
2018-05-15 12:05:30 -0300

    feat(str): Add Chainer and Splitter :bulb:
    I've added the Chainer and Splitter interfaces, that will operate
    through strings, using str.New function. At the moment it creates
    Chainer using fmt.Sprintf function, and can Split and Join strings.

---
+++ splitted.go
@@ -0,0 +1,55 @@
+package str
+
+import (
+	"io"
+	"strings"
+)
+
+// splitted represents a string array type with Factory pattern
+// behaviour.
+type splitted []string
+
+// Splitter it's a string array factory. It will mount strings arrays
+// through various methods, and then outputted in desired form.
+type Splitter interface {
+	Printer
+	Array() []string
+	Join(...string) Chainer
+}
+
+// Join will mout Splitted into a Chainer using separator received.
+// If nothing is received, it will use "" as separator. Uses
+// strings.Join on this operation.
+func (s splitted) Join(ss ...string) (c Chainer) {
+	if len(ss) == 0 {
+		c = chained(strings.Join([]string(s), ""))
+	} else {
+		c = chained(strings.Join([]string(s), ss[0]))
+	}
+	return
+}
+
+// Array returns the Splitter as a string array.
+func (s splitted) Array() (a []string) {
+	a = []string(s)
+	return
+}
+
+// String returns the Splitter as a string.
+func (s splitted) String() (r string) {
+	r = s.Join().String()
+	return
+}
+
+// Error returns a new error with Splitter as the message.
+func (s splitted) Error() (err error) {
+	err = s.Join().Error()
+	return
+}
+
+// Print will log Splitter content to writer received, or os.Stdout as
+// default writer.
+func (s splitted) Print(wa ...io.Writer) (n int, err error) {
+	n, err = s.Join().Print(wa...)
+	return
+}
